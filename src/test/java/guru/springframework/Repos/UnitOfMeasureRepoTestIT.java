eaf5e387a6953b0f9974abddddbcac16a780c02b
i-sokar <simosokar5@gmail.com>
2023-06-23 09:06:46 +0300

    added index test ctrl+repo integration test

---
+++ src/test/java/guru/springframework/Repos/UnitOfMeasureRepoTestIT.java
@@ -0,0 +1,32 @@
+package guru.springframework.Repos;
+
+import guru.springframework.Model.UnitOfMeasure;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepoTestIT extends TestCase {
+
+
+    @Autowired
+    UnitOfMeasureRepo unitOfMeasureRepo;
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+@Test
+    public void testFindByDescription() {
+    Optional<UnitOfMeasure> unitOfMeasure=unitOfMeasureRepo.findByDescription("Teaspoon");
+    assertEquals("Teaspoon",unitOfMeasure.get().getDescription());
+
+
+    }
+}

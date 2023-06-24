95797e3b4d80c2163efc2d2ccb1a1134f28aba93
i-sokar <simosokar5@gmail.com>
2023-06-24 07:06:13 +0300

    fixed issue
    jdk not compatible :D

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,8 +1,7 @@
-package guru.springframework.app;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
